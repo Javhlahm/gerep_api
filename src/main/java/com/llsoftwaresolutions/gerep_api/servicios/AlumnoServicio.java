72b9333d473c13955535b2ebe654f33de5f16793
Javier Lamas <gerardo1507@hotmail.com>
2025-06-19 14:48:52 -0600

    correccion de errores

--- src/main/java/com/llsoftwaresolutions/gerep_api/servicios/AlumnoServicio.java
+++ src/main/java/com/llsoftwaresolutions/gerep_api/servicios/AlumnoServicio.java
@@ -36,50 +36,52 @@ public class AlumnoServicio {
     public Alumno actualizarAlumno(Long id, Alumno alumnoActualizado) {
         Optional<Alumno> alumno = alumnoRepositorio.findById(id);
 
-       if (alumno.isPresent()) {
-    Alumno existente = alumno.get();
+        if (alumno.isPresent()) {
+            Alumno existente = alumno.get();
 
-    if (alumnoActualizado.getTagUid() != null && !alumnoActualizado.getTagUid().isEmpty()) {
-        existente.setTagUid(alumnoActualizado.getTagUid());
-    }
-    if (alumnoActualizado.getNombre() != null && !alumnoActualizado.getNombre().isEmpty()) {
-        existente.setNombre(alumnoActualizado.getNombre());
-    }
-    if (alumnoActualizado.getEdad() > 0) {
-        existente.setEdad(alumnoActualizado.getEdad());
-    }
-    if (alumnoActualizado.getFoto() != null && !alumnoActualizado.getFoto().isEmpty()) {
-        existente.setFoto(alumnoActualizado.getFoto());
-    }
-    if (alumnoActualizado.getPadres() != null && !alumnoActualizado.getPadres().isEmpty()) {
-        existente.setPadres(alumnoActualizado.getPadres());
-    }
-    if (alumnoActualizado.getContactoEmergencia() != null && !alumnoActualizado.getContactoEmergencia().isEmpty()) {
-        existente.setContactoEmergencia(alumnoActualizado.getContactoEmergencia());
-    }
-    if (alumnoActualizado.getHistorialMedico() != null && !alumnoActualizado.getHistorialMedico().isEmpty()) {
-        existente.setHistorialMedico(alumnoActualizado.getHistorialMedico());
-    }
-    if (alumnoActualizado.getTipoSangre() != null && !alumnoActualizado.getTipoSangre().isEmpty()) {
-        existente.setTipoSangre(alumnoActualizado.getTipoSangre());
-    }
-    if (alumnoActualizado.getCuidadosEspeciales() != null && !alumnoActualizado.getCuidadosEspeciales().isEmpty()) {
-        existente.setCuidadosEspeciales(alumnoActualizado.getCuidadosEspeciales());
-    }
-if (alumnoActualizado.getGrupo() != null && alumnoActualizado.getGrupo().getId() != null) {
-    existente.setGrupo(alumnoActualizado.getGrupo());
-}
+            if (alumnoActualizado.getTagUid() != null && !alumnoActualizado.getTagUid().isEmpty()) {
+                existente.setTagUid(alumnoActualizado.getTagUid());
+            }
+            if (alumnoActualizado.getNombre() != null && !alumnoActualizado.getNombre().isEmpty()) {
+                existente.setNombre(alumnoActualizado.getNombre());
+            }
+            if (alumnoActualizado.getEdad() > 0) {
+                existente.setEdad(alumnoActualizado.getEdad());
+            }
+            if (alumnoActualizado.getFoto() != null && !alumnoActualizado.getFoto().isEmpty()) {
+                existente.setFoto(alumnoActualizado.getFoto());
+            }
+            if (alumnoActualizado.getPadres() != null && !alumnoActualizado.getPadres().isEmpty()) {
+                existente.setPadres(alumnoActualizado.getPadres());
+            }
+            if (alumnoActualizado.getContactoEmergencia() != null
+                    && !alumnoActualizado.getContactoEmergencia().isEmpty()) {
+                existente.setContactoEmergencia(alumnoActualizado.getContactoEmergencia());
+            }
+            if (alumnoActualizado.getHistorialMedico() != null && !alumnoActualizado.getHistorialMedico().isEmpty()) {
+                existente.setHistorialMedico(alumnoActualizado.getHistorialMedico());
+            }
+            if (alumnoActualizado.getTipoSangre() != null && !alumnoActualizado.getTipoSangre().isEmpty()) {
+                existente.setTipoSangre(alumnoActualizado.getTipoSangre());
+            }
+            if (alumnoActualizado.getCuidadosEspeciales() != null
+                    && !alumnoActualizado.getCuidadosEspeciales().isEmpty()) {
+                existente.setCuidadosEspeciales(alumnoActualizado.getCuidadosEspeciales());
+            }
+            if (alumnoActualizado.getGrupo() != null && alumnoActualizado.getGrupo().getId() != null) {
+                existente.setGrupo(alumnoActualizado.getGrupo());
+            }
 
-    if (alumnoActualizado.getAsistencias() != null && !alumnoActualizado.getAsistencias().isEmpty()) {
-        existente.setAsistencias(alumnoActualizado.getAsistencias());
-    }
-    if (alumnoActualizado.getIncidencias() != null && !alumnoActualizado.getIncidencias().isEmpty()) {
-        existente.setIncidencias(alumnoActualizado.getIncidencias());
-    }
-    return alumnoRepositorio.save(existente);
-} else {
-    throw new RuntimeException("Usuario no Encontrado");
-}
+            if (alumnoActualizado.getAsistencias() != null && !alumnoActualizado.getAsistencias().isEmpty()) {
+                existente.setAsistencias(alumnoActualizado.getAsistencias());
+            }
+            if (alumnoActualizado.getIncidencias() != null && !alumnoActualizado.getIncidencias().isEmpty()) {
+                existente.setIncidencias(alumnoActualizado.getIncidencias());
+            }
+            return alumnoRepositorio.save(existente);
+        } else {
+            throw new RuntimeException("Usuario no Encontrado");
+        }
 
     }
 
--- src/main/java/com/llsoftwaresolutions/gerep_api/servicios/GrupoServicio.java
+++ src/main/java/com/llsoftwaresolutions/gerep_api/servicios/GrupoServicio.java
@@ -1,7 +1,9 @@
 package com.llsoftwaresolutions.gerep_api.servicios;
 
+import com.llsoftwaresolutions.gerep_api.entidades.Alumno;
 import com.llsoftwaresolutions.gerep_api.entidades.Grupo;
 import com.llsoftwaresolutions.gerep_api.entidades.Profesor;
+import com.llsoftwaresolutions.gerep_api.repositorios.AlumnoRepositorio;
 import com.llsoftwaresolutions.gerep_api.repositorios.GrupoRepositorio;
 import com.llsoftwaresolutions.gerep_api.repositorios.ProfesorRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,24 +15,26 @@ import java.util.Optional;
 @Service
 public class GrupoServicio {
 
-
     @Autowired
     private GrupoRepositorio grupoRepository;
 
     @Autowired
     private ProfesorRepositorio profesorRepository;
 
+    @Autowired
+    private AlumnoRepositorio alumnoRepository;
+
     public Grupo registrarGrupo(Grupo grupo) {
         Grupo grupoGuardado = grupoRepository.save(grupo);
 
-    if (grupoGuardado.getProfesor() != null) {
-        Long profesorId = grupoGuardado.getProfesor().getId();
-        Optional<Profesor> profesorCompleto = profesorRepository.findById(profesorId);
+        if (grupoGuardado.getProfesor() != null) {
+            Long profesorId = grupoGuardado.getProfesor().getId();
+            Optional<Profesor> profesorCompleto = profesorRepository.findById(profesorId);
 
-        profesorCompleto.get().setGrupo(grupoGuardado);
-        profesorRepository.save(profesorCompleto.get());
-    }
-    return grupoGuardado;
+            profesorCompleto.get().setGrupo(grupoGuardado);
+            profesorRepository.save(profesorCompleto.get());
+        }
+        return grupoGuardado;
     }
 
     public List<Grupo> obtenerTodosLosGrupos() {
@@ -46,18 +50,40 @@ public class GrupoServicio {
     }
 
     public Grupo actualizarGrupo(Long id, Grupo grupoActualizado) {
-        Optional<Grupo> grupo = grupoRepository.findById(id);
+        Optional<Grupo> grupoOpt = grupoRepository.findById(id);
 
-        if (grupo.isPresent()) {
-            grupo.get().setNombre(grupoActualizado.getNombre());
-            grupo.get().setDirector(grupoActualizado.getDirector());
-            grupo.get().setProfesor(grupoActualizado.getProfesor());
-            grupo.get().setAlumnos(grupoActualizado.getAlumnos());
+        if (grupoOpt.isPresent()) {
+            Grupo grupoExistente = grupoOpt.get();
 
-            return grupoRepository.save(grupo.get());
+            if (grupoActualizado.getNombre() != null && !grupoActualizado.getNombre().isEmpty()) {
+                grupoExistente.setNombre(grupoActualizado.getNombre());
+            }
+
+            if (grupoActualizado.getDirector() != null) {
+                grupoExistente.setDirector(grupoActualizado.getDirector());
+            }
+
+            if (grupoActualizado.getProfesor() != null) {
+                grupoExistente.setProfesor(grupoActualizado.getProfesor());
+
+                Profesor profesor = grupoActualizado.getProfesor();
+                profesor.setGrupo(grupoExistente);
+                profesorRepository.save(profesor);
+            }
+
+            if (grupoActualizado.getAlumnos() != null) {
+                grupoExistente.setAlumnos(grupoActualizado.getAlumnos());
+
+                for (Alumno alumno : grupoActualizado.getAlumnos()) {
+                    alumno.setGrupo(grupoExistente);
+                    alumnoRepository.save(alumno);
+                }
+            }
+
+            return grupoRepository.save(grupoExistente);
         } else {
-            throw new RuntimeException("Grupo no Encontrado");
+            throw new RuntimeException("Grupo no encontrado");
         }
-
     }
+
 }
